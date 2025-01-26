a9583b368b4e6b475ccb5dc307107b548be1bd99
YetAnotherFurryMan <damiansatala@gmail.com>
2025-01-26 21:26:11 +0000

    Removed dependency: sqlite3, moved to experimental build-in sqlite

--- core/model.js
+++ core/model.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const sqlite = require('node:sqlite');
 
-const sqlite3 = require('sqlite3').verbose();
-
 const database = new sqlite.DatabaseSync(":memory:");
 
 process.on('exit', function () {
